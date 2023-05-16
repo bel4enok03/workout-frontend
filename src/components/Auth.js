c6e7ff23413b6755bb5ce0e1f64ffa84baf53df2
bel4enok03 <belskaya-tanyush@mail.ru>
2023-05-16 16:11:43 +0300

    *

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -17,7 +17,7 @@ function Auth() {
 		e.preventDefault();
 		try {
 			const endpoint = isRegistration ? '/auth/register' : '/auth/login';
-			const response = await axios.post(`https://workout-backend-c6o4.onrender.com${endpoint}`, {
+			const response = await axios.post(`https://workout-backend-prq6.onrender.com${endpoint}`, {
 				email,
 				password,
 			});
